fd63d6a637f9dbfb030118d4962cd93be4b9f9d3
Arturo Suárez <masuareb@hotmail.com>
2021-02-04 20:57:07 -0600

    Initial  Commit

---
+++ src/test/java/dev/masuareb/thymeleafcrud/ThymeleafCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.masuareb.thymeleafcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
